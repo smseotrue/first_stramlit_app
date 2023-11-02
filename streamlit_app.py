b535bf89326217477457d41b40350030f9feee8f
smseotrue <149659043+smseotrue@users.noreply.github.com>
2023-11-02 17:13:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.text('Breakfast1')
-streamlt.text('Breakfast2')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
